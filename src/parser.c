a63616e47ea3636ed7a1575bde0938d81c9b0686
Muhammad Tirmazi <tirmazi@g.harvard.edu>
2020-04-07 22:31:25 -0400

    Updated code for parsing connect command

--- src/parser.c
+++ src/parser.c
@@ -4,7 +4,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void parser_connect(const char *cmd, const size_t len) {}
+static struct command_t parser_connect(const char *cmd, const size_t len) {
+  struct command_t parsed_cmd;
+  parsed_cmd.type = CMD_INVALID;
+
+  if (len == 0) {
+    return parsed_cmd;
+  }
+
+  if (*cmd != ' ') {
+    return parsed_cmd;
+  }
+
+  ++cmd;
+  --len;
+
+  if (len == 0) {
+    return parsed_cmd;
+  }
+
+  size_t counter = 0;
+
+  while (cmd[counter] != ' ' && counter < len) {
+    ++counter;
+  }
+
+  assert(counter < PARSER_BUF);
+
+  memset(parsed_cmd.info.connect.addr, '\0', PARSER_BUF);
+  memset(parsed_cmd.info.connect.opt, '\0', PARSER_BUF);
+
+  strncpy(parsed_cmd.info.connect.addr, cmd, counter);
+
+  parsed_cmd.type = CMD_CONNECT;
+
+  cmd += counter;
+  len -= counter;
+
+  if (len == 0) {
+    return parsed_cmd;
+  }
+
+  cmd++;
+  len--;
+
+  assert(len < PARSER_BUF);
+
+  strncpy(parsed_cmd.info.connect.opt, cmd, len);
+  return parsed_cmd;
+}
 
 struct command_t parser_next(void) {
 
@@ -35,6 +83,6 @@ struct command_t parser_next(void) {
   }
 
   struct command_t cmd;
-  cmd->type = CMD_INVALID;
+  cmd.type = CMD_INVALID;
   return cmd;
 }
